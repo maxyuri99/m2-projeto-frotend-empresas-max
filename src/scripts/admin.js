60bab79eb460425634f6f511c1e2a14f18460a8c
maxyuri99 <maxyurisantos@hotmail.com>
2023-07-12 15:23:33 -0300

    estilização e esqueleto do site montados

---
+++ src/scripts/admin.js
@@ -0,0 +1,11 @@
+// const buttonGambiarra = () =>{
+//     const button = document.querySelector('.admin__button-logout')
+
+//     button.addEventListener('click', () => {
+//         const modal = document.querySelector('.modal-remove-user__controller')
+
+//         modal.showModal()
+//     })
+// }
+
+// buttonGambiarra()
