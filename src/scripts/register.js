60f9a4075f0978e42af8c51b620d701544578091
maxyuri99 <maxyurisantos@hotmail.com>
2023-07-13 14:38:54 -0300

    feito todas funções e requests da pagina register

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,10 +1,11 @@
-import { authLogin } from "./request.js"
+import { registerEmployees } from "./request.js"
 import { createToastify } from "./toastify.js"
 
 const handleLogin = () => {
-    const inputs = document.querySelectorAll('.login__input')
-    const buttonAcess = document.querySelector('.login__button-acess')
-    const buttonRegister = document.querySelectorAll('.button-register')
+    const inputs = document.querySelectorAll('.register__input')
+    const buttonAcess = document.querySelector('.register__button-register')
+    const buttonLogin = document.querySelectorAll('.button-login')
+    const buttonHome = document.querySelector('.register')
     const spinner = document.querySelector('.spinner')
     let loginBody = {}
     let count = 0
@@ -24,23 +25,32 @@ const handleLogin = () => {
 
             return createToastify('Campo faltando', 'var(--alert100)')
         } else {
-            spinner.classList.remove('hidden__spinner')
+            spinner.classList.remove('hidden')
 
-            await authLogin(loginBody)
+            await registerEmployees(loginBody)
         }
     })
 
-    buttonRegister.forEach((button) => {
+    buttonLogin.forEach((button) => {
         button.addEventListener('click', () => {
-            location.replace('./register.html')
+            location.replace('./login.html')
         })
+    })
+
+    buttonHome.addEventListener('click', () => {
+        location.replace('../../index.html')
     })
 
 }
 
 const verifyInputsIncorrect = () => {
-    const inputEmail = document.getElementById('loginEmail')
-    const inputPassword = document.getElementById('loginPassword')
+    const inputEmail = document.getElementById('registerEmail')
+    const inputPassword = document.getElementById('registerPassword')
+    const inputName = document.getElementById('registerName')
+
+    inputName.addEventListener('input', () =>{
+        inputName.classList.remove('input__error-red')
+    })
 
     inputEmail.addEventListener('input', () =>{
         inputEmail.classList.remove('input__error-red')
